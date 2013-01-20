a6b0455158d725b352184bd99f161276ce74e037
edjafarov <djkojb@gmail.com>
2013-01-20 13:43:39 +0200

    vagrant hack

---
+++ vagrantstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE}")"
+git pull
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . /home/vagrant/
+}
+doIt
+unset doIt
+source /home/vagrant/.bash_profile
